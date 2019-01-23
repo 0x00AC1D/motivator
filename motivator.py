92fec3d7ec3429fce997354b6ba8e60645c07ead
Samuel Havelka <samuelhavelka@seznam.cz>
2019-01-23 05:56:13 +0100

    paths modified for pyinstaller

--- motivator.py
+++ motivator.py
@@ -3,14 +3,22 @@ from tkinter import ttk
 from playsound import playsound
 from PIL import ImageTk
 from threading import Thread
+import sys
+import os
+
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
 
 class App:
-    def __init__(self, root, window_title):
-        self.root = root
+    def __init__(self, window, window_title):
+        self.root = window
         self.root.title(window_title)
 
-        self.images = {"dothething": ImageTk.PhotoImage(file="dothething.gif"),
-                       "reward": ImageTk.PhotoImage(file="reward.gif")}
+        self.images = {"dothething": ImageTk.PhotoImage(file=resource_path("dothething.gif")),
+                       "reward": ImageTk.PhotoImage(file=resource_path("reward.gif"))}
 
         self.button_one = ttk.Button(self.root)
         self.button_two = ttk.Button(self.root, text="I have not yet done the thing",
@@ -26,9 +34,9 @@ class App:
 
     def play_sound(self, victory):
         if victory:
-            Thread(target=playsound, args=("victory.mp3",)).start()
+            Thread(target=playsound, args=(resource_path("victory.mp3"),)).start()
         else:
-            Thread(target=playsound, args=("sadtrombone.mp3",)).start()
+            Thread(target=playsound, args=(resource_path("sadtrombone.mp3"),)).start()
 
     def set_image(self, image):
         img = self.images[image]
@@ -50,6 +58,7 @@ class App:
         self.button_two.forget()
         self.status.configure(text="Good job! Reward yourself!")
         self.button_one.configure(text="I have another thing to do", command=self.do_the_thing)
+
 
 if __name__ == '__main__':
     root = tkinter.Tk()
